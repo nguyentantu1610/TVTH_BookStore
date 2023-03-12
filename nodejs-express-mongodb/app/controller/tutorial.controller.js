67993d31d641de7efaa0449a1a3b8a02c318e5e9
NTTu <tudeyer@gmail.com>
2023-03-12 23:11:26 +0700

    Update file tutorial.controller.js

--- nodejs-express-mongodb/app/controller/tutorial.controller.js
+++ nodejs-express-mongodb/app/controller/tutorial.controller.js
@@ -3,9 +3,18 @@ const Tutorial = db.tutorials;
 
 // Create and Save a new Tutorial
 exports.create = (req, res) => {
+    // Validate request
+    if (!req.body.title) {
+      res.status(400).send({ message: "Content can not be empty!" });
+      return;
+    }
   
-};
-
+    // Create a Tutorial
+    const tutorial = new Tutorial({
+      title: req.body.title,
+      description: req.body.description,
+      published: req.body.published ? req.body.published : false
+    });
 // Retrieve all Tutorials from the database.
 exports.findAll = (req, res) => {
   
