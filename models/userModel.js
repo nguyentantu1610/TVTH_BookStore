eaf8c6a384b0bf9039a1c5f9fbe3060d6b93550a
DESKTOP-HP8VKJ2\duong <duongminhhao23@gmail.com>
2023-03-18 23:13:27 +0700

    test database 2

--- models/userModel.js
+++ models/userModel.js
@@ -1,26 +1,40 @@
-const mongoose = require('mongoose'); // Erase if already required
+const mongoose = require('mongoose')
 
-// Declare the Schema of the Mongo model
-var userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
+        trim: true
     },
+
     email: {
         type: String,
         required: true,
-        unique: true,
+        unique: true
     },
+
     mobile: {
         type: String,
         required: true,
-        unique: true,
+        unique: true
     },
+
     password: {
         type: String,
         required: true,
     },
-});
 
-//Export the model
-module.exports = mongoose.model('User', userSchema);
+    role: {
+        type: Number,
+        default: 0
+    },
+
+    cart: {
+        type: Array,
+        default: []
+    }
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model('Users', userSchema)
