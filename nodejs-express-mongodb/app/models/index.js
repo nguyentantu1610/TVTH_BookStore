e47b431a7bc2e6b009c6480c493ebfca3f746396
NTTu <tudeyer@gmail.com>
2023-03-29 12:12:35 +0700

    Update file index.js

--- nodejs-express-mongodb/app/models/index.js
+++ nodejs-express-mongodb/app/models/index.js
@@ -1,4 +1,4 @@
-const dbConfig = require("../config/db.config.js");
+/*const dbConfig = require("../config/db.config.js");
 
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
@@ -8,4 +8,13 @@ db.mongoose = mongoose;
 db.url = dbConfig.url;
 db.tutorials = require("./index.js")(mongoose);
 
+module.exports = db;*/
+
+const mongoose = require("mongoose")
+mongoose.connect('mongodb://127.0.0.1:27017/db_09_10', { useNewUrlParser: true });
+
+const db = {}
+db.mongoose = mongoose
+db.url = "mongodb://127.0.0.1:27017/db_09_10"
+db.tutorials = require("./tutorial.model.js")(mongoose)
 module.exports = db;
